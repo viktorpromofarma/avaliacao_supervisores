e7d4969e2a6bf6873fa73a8c2e0685a0b61a0c2f
viktorpromofarma <viktor.santos@promofarma.com.br>
2025-02-12 17:57:35 -0300

    Feat criação da migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,10 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('display_name');
+            $table->integer('seller')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/migrations/2025_02_12_192131_questions_supervisors_assessment.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions_supervisors_assessment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->foreign('type_id')->references('id')->on('type_questions_supervisors_assessment');
+            $table->foreign('category_id')->references('id')->on('category_questions_supervisors_assessment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
