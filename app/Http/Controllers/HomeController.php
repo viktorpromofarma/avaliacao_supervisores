cb3ccfa6da033c619b0a771d3967791fb686f5f0
viktorpromofarma <viktor.santos@promofarma.com.br>
2025-02-14 17:37:14 -0300

    test: criada as regras de acesso temporarias

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\Sellers;
 
 class HomeController extends Controller
 {
+    public $roules = '';
+
     public function index()
     {
+        $roules = $this->getRoules();
+        return view('home', ['roules' => $roules]);
+    }
 
-        return view('home', ['teste' => 'viktor']);
+    public function getRoules()
+    {
+        if (Sellers::where('supervisor', Auth::user()->seller)->Exists()) {
+            return $this->roules = 'supervisor';
+        } elseif (Sellers::where('gerente_atual', Auth::user()->seller)->Exists()) {
+            return $this->roules = 'gerente';
+        } elseif (Auth::user()->seller == 4971) {
+            return $this->roules = 'admin';
+        };
     }
 }
