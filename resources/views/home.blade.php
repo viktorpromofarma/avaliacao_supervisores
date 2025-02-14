c4c95adbe5f1c3ee9273981590856dc0a30773e6
viktorpromofarma <viktor.santos@promofarma.com.br>
2025-02-14 17:36:47 -0300

    test: adicionado os teste de acesso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,43 @@
 <x-mains.navigation />
 <x-mains.app>
-    <div style="margin-top: 10%;">
-        <x-navigation.fieldset legend="Área do Gerente">
-            <x-navigation.buttomModal action="openConfirmation()" description="Realizar Avaliação"
-                icon="<i class='fa-regular fa-pen-to-square fa-5x'></i>" />
 
-            <x-navigation.options route="{{ route('home') }}" icon="<i class='fa-solid fa-file-circle-check fa-5x'></i>"
-                description="Avaliações Realizadas" />
+    <div style="margin-top: 5%; ">
+
+        <x-navigation.fieldset
+            legend="{{ $roules == 'gerente'
+                ? 'Área do Gerente'
+                : ($roules == 'supervisor'
+                    ? 'Área do Supervisor'
+                    : 'Área do Admin') }}">
+
+
+            @if ($roules == 'gerente')
+                <x-navigation.buttomModal action="openConfirmation()" description="Realizar Avaliação"
+                    icon="<i class='fa-regular fa-pen-to-square fa-4x'></i>" />
+                <x-navigation.options route="#" icon="<i class='fa-solid fa-file-circle-check fa-4x'></i>"
+                    description="Avaliações Realizadas" />
+            @elseif ($roules == 'supervisor')
+                <x-navigation.options route="#" icon="<i class='fa-solid fa-chart-line fa-4x'></i>"
+                    description="Notas de Avaliação" />
+                <x-navigation.options route="#" icon="<i class='fa-regular fa-comments fa-4x'></i>"
+                    description="Feedbacks Aplicados" />
+            @elseif($roules == 'admin')
+                <div class="grid grid-cols-3 gap-4 md:grid-cols-3">
+                    <x-navigation.options route="#" icon="<i class='fa-solid fa-file-pen fa-4x'></i>"
+                        description="Gerar Feedback" />
+                    <x-navigation.options route="#" icon="<i class='fa-solid fa-user-clock fa-4x'></i>"
+                        description="Histórico de avaliações" />
+                    <x-navigation.options route="#" icon="<i class='fa-regular fa-calendar-days fa-4x'></i>"
+                        description="Histórico de feedbacks" />
+                    <x-navigation.options route="#" icon="<i class='fa-solid fa-screwdriver-wrench fa-4x'></i>"
+                        description="Períodos da avaliação" />
+                    <x-navigation.options route="#" icon="<i class='fa-solid fa-list-ol fa-4x'></i>"
+                        description="Cadastrar Categorias" />
+                    <x-navigation.options route="#" icon="<i class='fa-solid fa-list-check fa-4x'></i>"
+                        description="Cadastrar questões" />
+                </div>
+            @endif
+
         </x-navigation.fieldset>
 
         <x-modals.confirmation action="closeConfirmation()" description="Deseja realizar a avaliação?"
