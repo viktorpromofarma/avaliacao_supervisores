d0d04d3eda684b413edcdc096271b35f5dd34dd2
viktorpromofarma <viktor.santos@promofarma.com.br>
2025-02-14 17:35:57 -0300

    feat: Adicionado link de esqueceu a senha

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -1,6 +1,6 @@
 <x-mains.app>
     <div class="flex items-center justify-center min-h-screen" style="background-color: #fff8f9">
-        <div class="max-w-md p-6 bg-white border border-gray-300 rounded-lg shadow-xl w-62 ">
+        <div class="max-w-md p-6 bg-white border border-red-600 rounded-lg shadow-xl w-62 ">
             @if ($message = Session::get('error'))
                 <div class="mb-4 text-sm text-red-600"> {{ $message }} </div>
             @endif
@@ -34,7 +34,25 @@
                     <x-inputs.label for="password" text="Sua Senha" class="block font-bold text-gray-700" />
                     <x-inputs.input id="password" name="password" type="password" placeholder="" />
                 </div>
+                <!-- Esqueci a senha -->
+                <div class="mt-4 text-center">
+                    <div class="relative group">
+                        <!-- Link com o texto "Esqueceu a senha?" -->
+                        <a href="http://app.promofarma.int/helpdesk/" class="font-bold text-red-500" target="_blank">
+                            Esqueceu a senha ?
+                        </a>
+
+                        <!-- Tooltip que aparece ao passar o mouse -->
+                        <div
+                            class="absolute invisible px-3 py-2 mb-2 text-sm text-white transition-opacity duration-300 transform -translate-x-1/2 bg-gray-800 rounded opacity-0 bottom-full left-1/2 group-hover:opacity-100 group-hover:visible">
+                            Clique aqui para abrir um chamado e recuperar sua senha.
+                        </div>
+                    </div>
+                </div>
+
+
                 <!-- Botão de Login -->
+
                 <div class="mt-4 text-center">
                     <x-inputs.button type="submit" text="Acessar sua conta" textColor="white"
                         backgroundColor="#E2304E" />
